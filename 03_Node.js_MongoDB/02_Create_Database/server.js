768ac12b7e22de36b74c864994cb30ae68ba51e2
danielOuattara <daniel.ouattara@gmx.com>
2022-11-11 19:11:51 +0100

    start updating: no more callback in mongodb driver

--- 03_Node.js_MongoDB/02_Create_Database/server.js
+++ 03_Node.js_MongoDB/02_Create_Database/server.js
@@ -1,5 +1,3 @@
-
-
 /* Creating a Database
 ======================
 To create a database in MongoDB, start by creating 
@@ -10,18 +8,24 @@ the database you want to create.
 MongoDB will create the database if it does not exist, 
 and make a connection to it. */
 
-const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/w3schools_nodejs_database";
+const { MongoClient } = require("mongodb");
+const url = "mongodb://localhost:27017/";
+const client = new MongoClient(url);
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
-
+client
+  .connect()
+  .then(() => {
+    const database = client.db("w3schools_nodejs_database");
+    return database.createCollection("customers_10");
+  })
+  .then(() => {
+    console.log(" Collection created !");
+    client.close();
+  })
+  .catch((err) => console.log(err));
 
 /* Important: In MongoDB, a database is not created until 
-              it gets content!
+it gets content!
 
 MongoDB waits until you have created a collection (table), 
 with at least one document (record) before it actually creates 
--- 03_Node.js_MongoDB/03_Creating_Collection/server.js
+++ 03_Node.js_MongoDB/03_Creating_Collection/server.js
@@ -1,24 +1,21 @@
-
 /* Creating a Collection
 =========================
 
 To create a collection in MongoDB, use the 
 "createCollection()" method: */
 
-const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/";
+// const MongoClient = require("mongodb").MongoClient;
+// const url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  const database = db.db("w3schools_nodejs_database");
-  database.createCollection("customers", (err, res)=> {
-    if (err) throw err;
-    console.log(" Collection created !")
-  })
-  
-  db.close();
-});
-
+// MongoClient.connect(url, function (err, db) {
+//   if (err) throw err;
+//   const database = db.db("w3schools_nodejs_database");
+//   database.createCollection("customers", (err, res) => {
+//     if (err) throw err;
+//     console.log(" Collection created !");
+//     db.close();
+//   });
+// });
 
 /* Important: In MongoDB, a collection is not created until 
               it gets content!
@@ -26,4 +23,44 @@ MongoClient.connect(url, function(err, db) {
 MongoDB waits until you have inserted a document before 
 it actually creates the collection.
 
-. */
+*/
+
+// ---------------------------------------------------------
+
+// const MongoClient = require("mongodb").MongoClient;
+// const url = "mongodb://localhost:27017/";
+// const client = new MongoClient(url);
+
+// MongoClient.connect(url)
+//   .then((client) => {
+//     const database = client.db("w3schools_nodejs_database");
+//     return database;
+//   })
+//   .then((database) => {
+//     return database.createCollection("customers_2");
+//   })
+//   .then(() => {
+//     console.log(" Collection created !");
+//     client.close();
+//   })
+//   .catch((err) => console.log(err));
+
+// ---------------------------------------------------------
+
+const { MongoClient } = require("mongodb");
+const url = "mongodb://localhost:27017/";
+const client = new MongoClient(url);
+
+client
+  .connect()
+  .then(() => {
+    const database = client.db("w3schools_nodejs_database");
+    return database.createCollection("customers_");
+  })
+  .then(() => {
+    console.log(" Collection created !");
+    client.close();
+  })
+  .catch((err) => console.log(err));
+
+// ---------------------------------------------------------
